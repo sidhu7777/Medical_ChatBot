68a0ec632d8abd73cb279ab36434e09e2a873844
vb2017 <bvineeth76@gmail.com>
2025-03-19 20:52:27 +0000

    project structue updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name='Generative AI Project',
+    version= '0.0.1',
+    author= 'Vineeth',
+    author_email='bvineeth76@gmail.com',
+    packages= find_packages(),
+    install_requires = [] 
+
+    )
